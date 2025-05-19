b039606d5ff924dab064183c7e1dc8af0ab7ed12
duclee193 <ducorgan2003@gmail.com>
2025-05-20 01:23:30 +0700

    ok2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -47,4 +48,4 @@ def index():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
